eec90d2715166134e2323a0f48dc7b17ecc4d3e4
tvaditya <tvaditya@gmail.com>
2021-05-22 10:44:31 -0300

    pytest modifications

--- backend/core/config_test.py
+++ backend/core/config_test.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-env_path = Path('.')/'.env'
+env_path = Path('.')/'.env_test'
 load_dotenv(dotenv_path=env_path)
 
 class Settings:
--- backend/tests/test_routes/test_users.py
+++ backend/tests/test_routes/test_users.py
@@ -4,6 +4,7 @@ import json
 def test_create_user(client):
     data = {"username":"testuser","email":"testuser@nofoobar.com","password":"testing"}
     response = client.post("/user/",json.dumps(data))
+    print("test with sqllite")
     assert response.status_code == 200
     assert response.json()["email"] == "testuser@nofoobar.com"
     assert response.json()["is_active"] == True
