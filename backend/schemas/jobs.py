03ee3bbdea83d5ddec2105b850bedd6e64cdd5e4
tvaditya <tvaditya@gmail.com>
2021-05-26 16:26:25 -0300

    Delete a job by ID created

--- backend/schemas/jobs.py
+++ backend/schemas/jobs.py
@@ -1,20 +1,23 @@
 from typing import Optional
 from pydantic import BaseModel
-from datetime import date, datetime
+from datetime import date,datetime
+
 
 class JobBase(BaseModel):
     title: Optional[str] = None
-    company: Optional[str] = None
+    company: Optional[str] =None
     company_url: Optional[str] = None
-    location: Optional[str] = None
-    description: Optional[str] = "Remote"
+    location: Optional[str] = "Remote"
+    description: Optional[str] = None
     date_posted: Optional[date] = datetime.now().date()
+
 
 class JobCreate(JobBase):
     title: str
     company: str
     location: str
     description: str
+
 
 class ShowJob(JobBase):
     title: str
