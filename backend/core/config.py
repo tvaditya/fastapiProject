77e5450ced46c8418a42947ff3254fe42ce1cf77
tvaditya <tvaditya@gmail.com>
2021-05-27 16:38:33 -0300

    Authentication implemented

--- backend/core/config.py
+++ backend/core/config.py
@@ -14,5 +14,8 @@ class Settings:
     POSTGRES_PORT=os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB=os.getenv("POSTGRES_DB","db_jobboard")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 15
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
 
 settings = Settings()
