3b8c287a2bf28eca25a6140c18d00684cd222513
tvaditya <tvaditya@gmail.com>
2021-05-29 11:47:20 -0300

    image added to navbar

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from db.session import engine
 from db.base import Base
 from apis.base import api_router
 from webapps.base import api_router as webapp_router
+from fastapi.staticfiles import StaticFiles
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
@@ -12,11 +13,15 @@ def include_router(app):
     app.include_router(api_router)
     app.include_router(webapp_router)
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     create_tables()
     include_router(app)
+    configure_static(app)
     return app
 
 
