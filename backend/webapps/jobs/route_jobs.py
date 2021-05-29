2e1416e99f99a3b76deefb38779ba07b9d9339ec
tvaditya <tvaditya@gmail.com>
2021-05-29 12:41:45 -0300

    list view created

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -2,11 +2,17 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
+
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
 
 @router.get("/")
-def home(request: Request):
+def home(request: Request, db:Session = Depends(get_db)):
     print(dir(request))
-    return templates.TemplateResponse("jobs/homepage.html", {"request": request})
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html", {"request": request, "jobs": jobs})
