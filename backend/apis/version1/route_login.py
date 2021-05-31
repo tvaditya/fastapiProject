4255090f932b1a034ed14a159b19ced0db7a14e6
tvaditya <tvaditya@gmail.com>
2021-05-31 16:14:58 -0300

    User authentication and job form created

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,10 +1,11 @@
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from jose import jwt, JWTError
+from apis.utils import OAuth2PasswordBearerWithCookie
 
 from db.session import get_db
 from db.repository.login import get_user
@@ -25,12 +26,13 @@ def authenticate_user(username: str, password: str, db: Session):
 
 
 @router.post("/token")
-def login_for_access_token(form_data: OAuth2PasswordRequestForm=Depends(), db:Session = Depends(get_db)):
+def login_for_access_token(response: Response, form_data: OAuth2PasswordRequestForm=Depends(), db:Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     access_token_expire = timedelta(minutes= settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub":  user.email}, expires_delta=access_token_expire)
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
     return {"access_token": access_token, "token_type": "bearer"}
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
